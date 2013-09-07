eb13191f9d31fcaef5841d80ee012c1111b54159
alejandrof <alejandro@zauberlabs.com>
2013-09-07 14:04:08 -0300

    contador minimizado

--- js/jquery.countdown.js
+++ js/jquery.countdown.js
@@ -16,7 +16,7 @@ function Countdown() {
 	this.regional = []; // Available regional settings, indexed by language code
 	this.regional[''] = { // Default regional settings
 		// The display texts for the counters
-		labels: ['Years', 'Months', 'Weeks', 'Days', 'Hours', 'Minutes', 'Seconds'],
+		labels: ['Years', 'Months', 'Weeks', 'Días', 'Horas', 'Min.', 'Sec.'],
 		// The display texts for the counters if only one
 		labels1: ['Year', 'Month', 'Week', 'Day', 'Hour', 'Minute', 'Second'],
 		compactLabels: ['y', 'm', 'w', 'd'], // The compact texts for the counters
